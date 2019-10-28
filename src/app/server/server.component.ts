20be7645978337a2bc05f8fd7ac878e5e7dcc042
Anto <ganto1812@gmail.com>
2019-10-28 17:54:54 -0300

    Strings interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-	selector: 'app-server',
-	templateUrl: './server.component.html'
+  selector: 'app-server',
+  templateUrl: './server.component.html'
 })
 export class ServerComponent {
+  serverID = 10;
+  serverStatus = 'Offline';
 
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
